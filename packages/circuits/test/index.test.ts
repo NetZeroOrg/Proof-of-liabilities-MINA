7107fc0a91af0d51a57f927a1b14e32530ebcecf
utkarshdagoat <utkarsh382004@gmail.com>
2025-02-17 13:14:30 +0530

    feat: init

---
+++ packages/circuits/test/index.test.ts
@@ -0,0 +1,8 @@
+import { addTwoNumbersProgram } from "../src/programs/addTwoNumbers"
+
+describe("Add two number test", () => {
+    it("Should compile", async () => {
+        const { verificationKey } = await addTwoNumbersProgram.compile()
+        console.log(verificationKey)
+    })
+})
